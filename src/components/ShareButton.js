4719e99bcdbfb6de86e4731f450cd04ae6d04a0f
Enrique <enriquec@mit.edu>
2022-09-06 11:04:33 -0400

    Rudimentary Twitter share button

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -5,9 +5,16 @@ import {faInstagram, faLinkedinIn, faSquareFacebook, faTwitter} from "@fortaweso
 import {faCopy} from "@fortawesome/free-solid-svg-icons";
 import {default as _SHARE} from "../img/share.svg";
 
+
+const SHARE_HASHTAGS = ["nyc", "spatialequity"]
+const TWEET_INTENT_URL = "https://twitter.com/intent/tweet"
+
 export default function ShareButton({}) {
 
     const [clicked, setClicked] = useState(false)
+    const [shareText, setShareText] = useState("Check out this site:")
+    const [shareUrl, setShareUrl] = useState("https://nyc25x25.org/")
+
 
     const copyURL = () => {
         navigator.clipboard.writeText(window.location.href)
@@ -57,14 +64,15 @@ export default function ShareButton({}) {
                                      }
                                  }
                 />
-                <FontAwesomeIcon icon={faTwitter}
-                                 onClick={
-                                     () => {
-                                         setClicked(false)
-                                         uploadTwitter()
-                                     }
-                                 }
-                />
+                {/* https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview */}
+                <a
+                    class="twitter-share-button"
+                    target="_blank"
+                    href={`${TWEET_INTENT_URL}?text=${encodeURIComponent(shareText)}&url=${shareUrl}&hashtags=${SHARE_HASHTAGS.join(",")}`}
+                    onClick={() => setClicked(false)}
+                >
+                    <FontAwesomeIcon icon={faTwitter}/>
+                </a>
                 <FontAwesomeIcon icon={faInstagram}
                                  onClick={
                                      () => {
