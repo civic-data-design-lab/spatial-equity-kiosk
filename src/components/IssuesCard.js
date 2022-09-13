e3602ac9a1818b7f3e28d90883d96797d52f4c7e
NIKO <39039032+niko-dellic@users.noreply.github.com>
2022-09-12 21:37:22 -0400

    language and bug fix

--- src/components/IssuesCard.js
+++ src/components/IssuesCard.js
@@ -29,51 +29,21 @@ export default function IssuesCard({
     );
   };
 
-  const health_issues = issues.issues_data['health'].specific_issues_ID.map(
-    (id_) => {
-      return issues.specific_issues_data[id_];
-    }
-  );
-
-  const environment_issues = issues.issues_data[
-    'environment'
-  ].specific_issues_ID.map((id_) => {
-    return issues.specific_issues_data[id_];
-  });
-
-  const infrastructure_issues = issues.issues_data[
-    'infrastructure'
-  ].specific_issues_ID.map((id_) => {
-    return issues.specific_issues_data[id_];
-  });
-
-  const getRankingNarrative = (items, obj) => {
+  const getRankingNarrative = (obj) => {
     if (selectedCommunity) {
       const subject = obj.json_id;
       const fullIssueName = obj.specific_issue_name;
-      const possible_keys = items.map((item) => {
-        return item.specific_issue_ID;
-      });
 
-      const lastItem = selectedSpecificIssue
-        ? boundary == 'council'
-          ? _RANKINGS.council[
-              issues.specific_issues_data[selectedSpecificIssue].json_id
-            ].length
-          : _RANKINGS.community[
-              issues.specific_issues_data[selectedSpecificIssue].json_id
-            ].length
-        : null;
+      const lastItem = boundary == 'council' ? '51' : '59';
 
-      const metricRanking = selectedSpecificIssue
-        ? boundary == 'council'
+      const metricRanking =
+        boundary == 'council'
           ? _RANKINGS.council[subject].find(
               (f) => f.community_ID == selectedCommunity.json_lookup
             ).rank
           : _RANKINGS.community[subject].find(
               (f) => f.community_ID == selectedCommunity.json_lookup
-            ).rank
-        : '';
+            ).rank;
 
       const boundaryGrammatical =
         boundary == 'council'
@@ -87,30 +57,6 @@ export default function IssuesCard({
       return (
         <p>
           {` ${boundaryGrammatical} ranks ${metricRanking} out of ${lastItem} citywide in ${fullIssueName}.`}
-        </p>
-      );
-      return (
-        <p className={'mb-3 small-font'}>
-          {selectedCommunity.name} ranks {metricRanking}
-          {`out of ${boundary == 'council' ? '51' : '59'} `}
-          {`districts citywide for `}
-          <strong>
-            {issues.specific_issues_data[selectedSpecificIssue]
-              .issue_hi_low[0] || ''}
-            {` ${issues.specific_issues_data[
-              selectedSpecificIssue
-            ].specific_issue_name.toLowerCase()} 
-                ${
-                  issues.specific_issues_data[selectedSpecificIssue]
-                    .issue_units_shorthand != ''
-                    ? issues.specific_issues_data[
-                        selectedSpecificIssue
-                      ].issue_units_shorthand.toLowerCase()
-                    : issues.specific_issues_data[
-                        selectedSpecificIssue
-                      ].specific_issue_units.toLowerCase()
-                }.`}
-          </strong>
         </p>
       );
     }
@@ -144,11 +90,14 @@ export default function IssuesCard({
               setShowInfo(false);
             }}
           >
-            <FontAwesomeIcon icon={faCircleInfo} />
+            <FontAwesomeIcon
+              style={{ cursor: 'pointer' }}
+              icon={faCircleInfo}
+            />
             <div
               className={`${
                 showInfo ? '' : 'd-none'
-              } position-absolute info-tooltip`}
+              } position-absolute info-tooltip smaller-text`}
             >
               <p className={'m-0'}>
                 {
@@ -160,6 +109,7 @@ export default function IssuesCard({
           </div>
           {forMoreIssues && (
             <FontAwesomeIcon
+              style={{ cursor: 'pointer' }}
               icon={faXmark}
               onClick={(e) => {
                 e.stopPropagation();
@@ -179,10 +129,7 @@ export default function IssuesCard({
       <div className={'issues-card-body'}>
         <div>VISUALIZATION</div>
         <div className={'m-0 small-text'}>
-          {getRankingNarrative(
-            infrastructure_issues,
-            issues.specific_issues_data[specificIssue]
-          )}{' '}
+          {getRankingNarrative(issues.specific_issues_data[specificIssue])}{' '}
         </div>
       </div>
     </div>
