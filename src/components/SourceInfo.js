2aa654bd672ee752d287d396ad202898d03b4cb5
NIKO <39039032+niko-dellic@users.noreply.github.com>
2022-10-04 14:32:46 -0400

    added question mark to source info

--- src/components/SourceInfo.js
+++ src/components/SourceInfo.js
@@ -46,48 +46,18 @@ export default function MapTooltip({
         }`}
       >
         <p className={'m-0'}>
-          {`Source: ${issues.specific_issues_data[selectedSpecificIssue].specific_issue_source}. ${issues.specific_issues_data[selectedSpecificIssue].year}.
+          {`Source: ${issues.specific_issues_data[selectedSpecificIssue]?.specific_issue_source}. ${issues.specific_issues_data[selectedSpecificIssue]?.year}.
             `}
         </p>
         <span className="text-secondary fst-italic fw-light">
           {issues.specific_issues_data[selectedSpecificIssue]
-            .methodology_warning
+            ?.methodology_warning
             ? `*
-           ${issues.specific_issues_data[selectedSpecificIssue].methodology_warning}.
+           ${issues.specific_issues_data[selectedSpecificIssue]?.methodology_warning}.
            Click to learn more.`
             : 'Click to learn more.'}
         </span>
       </div>
     </div>
   );
-}
-
-{
-  /* <div
-  onMouseEnter={() => {
-    setShowInfo(true);
-  }}
-  onMouseLeave={() => {
-    setShowInfo(false);
-  }}
-  onClick={(e) => {
-    e.stopPropagation();
-    setSelectedChapter(4);
-    setSelectedAbout(9);
-  }}
->
-  <FontAwesomeIcon style={{ cursor: 'pointer' }} icon={faCircleInfo} />
-  <div
-    className={`${
-      showInfo ? '' : 'd-none'
-    } position-absolute info-tooltip smaller-text end-0`}
-  >
-    <p className={'m-0'}>
-      {issues.specific_issues_data[specificIssue].specific_issue_source}
-      {'. '}
-      {issues.specific_issues_data[specificIssue].year}
-      {'.'}
-    </p>
-  </div>
-</div>; */
 }
