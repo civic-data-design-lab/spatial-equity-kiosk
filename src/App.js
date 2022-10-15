369dd577c36f0790219b0e63a33a1ec7ec69564f
Kelly Fang <kellyfangg@gmail.com>
2022-10-15 12:12:34 -0400

    body and html height 100%

--- src/App.js
+++ src/App.js
@@ -868,7 +868,7 @@ function App() {
                       greenspaces — is designed, distributed, and accessed.
                       Browse citywide data or search community profiles to learn
                       how decisions about the use of public space lead to
-                      unequal outcomes and what you can do about it. [testing2]
+                      unequal outcomes and what you can do about it. [testing3]
                     </p>
                   </div>
                 </div>
